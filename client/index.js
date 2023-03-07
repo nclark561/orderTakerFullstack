fbc8be93a1e5aa7ef00092b7d1f29129b2752175
Noah Clark <noahammon00@gmail.com>
2023-03-07 11:24:18 -0700

    fixed bug where both forms had same id to get frontend and backend successfully working together

--- client/index.js
+++ client/index.js
@@ -1,10 +1,19 @@
 const form = document.querySelector('#menu-form');
+const editForm = document.querySelector('#order-edit');
 const nameInput = document.querySelector('#nameInput');
 const entreeIn = document.querySelector('#entreeInput');
 const sideIn = document.querySelector('#sideInput');
 const drinkIn = document.querySelector('#drinkInput');
 const radioIn = document.getElementsByName('togo');
 const pickupIn = document.querySelector('#pickupInput');
+const idIn = document.querySelector('#order-id')
+const editName = document.querySelector('#edit-name');
+const editEntree = document.querySelector('#edit-entree');
+const editSide = document.querySelector('#edit-side');
+const editDrink = document.querySelector('#edit-drink');
+const editRadio = document.getElementsByName('edit-togo');
+const editPickup = document.querySelector('#edit-pickup');
+let allOrders = [];
 
 const radioBtnVal = () => {
     for (let i = 0; i < radioIn.length; i++) {
@@ -13,6 +22,15 @@ const radioBtnVal = () => {
         }
     }
 }
+
+const radioBtnVal2 = () => {
+    for (let i = 0; i < editRadio.length; i++) {
+        if (editRadio[i].checked) {
+            return editRadio[i].value;
+        }
+    }
+}
+
 
 class Order {
     constructor(name, entree, side, drink, togo, pickup) {
@@ -36,6 +54,8 @@ const addOrder = evt => {
     
     axios.post('http://localhost:9001/order', guestOrder).then((res) => {
         alert(`Order submitted. Your order number is ${guestOrder.id}`);
+        console.log(res.data);
+        allOrders = res.data;
     }).catch(() => console.log('issue with add order function'));
 
     nameInput.value = '';
@@ -46,4 +66,34 @@ const addOrder = evt => {
     pickupIn.value = '8';
 };
 
+const editOrder = evt => {
+    evt.preventDefault();
+
+    let radioVal = radioBtnVal2();
+
+    for (let i = 0; i < allOrders.length; i++){
+        if (allOrders[i].id === +idIn.value) {
+            let guestOrder = allOrders[i];
+            guestOrder.name = editName.value;
+            guestOrder.entree = editEntree.value;
+            guestOrder.side = editSide.value;
+            guestOrder.drink = editDrink.value;
+            guestOrder.togo = radioVal;
+            guestOrder.pickup = editPickup.value;
+            axios.put('http://localhost:9001/order/edit', guestOrder).then((res) => {
+            allOrders = res.data;
+            }).catch((err) => console.log(err));
+            console.log(allOrders);
+            break;
+        }
+    }
+    editName.value = '';
+    editEntree.value = '';
+    editSide.value = '';
+    editDrink.value = 'mtn-dew';
+
+    editPickup.value = '8';
+}
+
 form.addEventListener('submit', addOrder);
+editForm.addEventListener('submit', editOrder);
--- server/index.js
+++ server/index.js
@@ -10,8 +10,24 @@ let allOrders = [];
 
 app.post('/order', (req, res) => {
     allOrders.push(req.body);
-    res.status(200).send('Order successfully recieved.');
     console.log(allOrders);
+    res.status(200).send(allOrders);
+})
+
+app.put('/order/:edit', (req, res) => {
+    for (let i = 0; i < allOrders.length; i++) {
+        if (allOrders[i].id === req.body.id) {
+            allOrders[i].name = req.body.name;
+            allOrders[i].entree = req.body.entree;
+            allOrders[i].side = req.body.side;
+            allOrders[i].drink = req.body.drink;
+            allOrders[i].togo = req.body.togo;
+            allOrders[i].pickup = req.body.pickup;
+            console.log(allOrders);
+            res.status(200).send(allOrders);
+            break;
+        }
+    }
 })
 
 app.listen(9001, () => console.log('server running on port 9001'));
